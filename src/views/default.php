4ecf7700abcd0c19f846c66c23f9bccf2123f38c
GHopper <np25071984@gmail.com>
2017-12-07 17:13:15 +0300

    README.md update

--- src/views/default.php
+++ src/views/default.php
@@ -1,5 +1,5 @@
 <div class="svg_map">
-    <div id="tooltip<?= $id ?>">tooltip</div>
+    <div id="tooltip<?= $id ?>"></div>
     <svg id="mapSVG<?= $id ?>" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" />
 </div>
 
