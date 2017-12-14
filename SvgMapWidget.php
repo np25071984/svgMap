6a7df5d33bb002e852c828989f93dbf00a40fd76
Alexey Shevchenko <ivanovosity@gmail.com>
2017-12-15 01:14:45 +0300

    Return to master

--- SvgMapWidget.php
+++ SvgMapWidget.php
@@ -3,6 +3,9 @@
 namespace ghopper\svgmap;
 
 use yii\base\Widget;
+use yii\web\JsExpression;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 
 /**
  * Class SvgMapWidget
@@ -13,8 +16,10 @@ class SvgMapWidget extends Widget
     const DATA_SOURCE_ARRAY = 1;
     const DATA_SOURCE_JSON_URL = 2;
 
+    public $id;
+
     /**
-     * @var int Data provider type
+     * @var int data provider type
      */
     public $type;
 
@@ -24,59 +29,120 @@ class SvgMapWidget extends Widget
     public $data;
 
     /**
-     * @var bool Tooltip show flag
+     * @var bool ToolTip show flag
      */
-    public $showTip;
+    public $showTip = true;
 
     /**
-     * @var bool Tools panel show flag
+     * Options ToolTip
+     * ['position' => ['x' => 80, 'y' => 120]],
+     * @var array
      */
-    public $showTools;
-
+    public $toolTipOptions = [];
 
     /**
-     * @var string Callback functions for events handler
+     * HTML tag options container
+     * @var array
+     */
+    public $containerOptions = ['class' => 'svg_map'];
+
+    /**
+     * HTML tag options tooltip container
+     * @var array
+     */
+    public $toolTipContainerOptions = [];
+
+    /**
+     * HTML tag svg options container
+     * @var array
+     */
+    public $svgContainerOptions = [];
+
+    /**
+     * @var string callback function for event handler
      */
     public $onClick;
     public $onOver;
     public $onOut;
 
+    private $toolsId;
+
     public function init()
     {
         parent::init();
+
+        $this->id = $this->id ?: $this->getId();
+        $tooltipId = 'tooltip_' . $this->id;
+        $svgId = 'mapSVG_' . $this->id;
+        $toolsId = 'tools_' . $this->id;
+
+        $this->onClick = ($this->onClick) ? $this->onClick : 'null';
+        $this->onOver = ($this->onOver) ? $this->onOver : 'null';
+        $this->onOut = ($this->onOut) ? $this->onOut : 'null';
 
         if ($this->type === self::DATA_SOURCE_ARRAY) {
             $this->data = json_encode($this->data);
         }
 
-        /**
-         * Show tooltip by default
-         */
-        if (!isset($this->showTip)) {
-            $this->showTip = true;
-        }
+        $this->type = ($this->type === self::DATA_SOURCE_ARRAY) ? 'json' : 'url';
 
-        /**
-         * Hide tools by default
-         */
-        if (!isset($this->showTools)) {
-            $this->showTools = false;
-        }
+        // Html опции для контейнера toolTip
+        $this->toolTipContainerOptions = ArrayHelper::merge([
+            'id' => $tooltipId,
+            'class' => 'svg_map_tooltip',
+        ], $this->toolTipContainerOptions);
+        $this->toolTipContainerOptions['id'] = $this->toolTipContainerOptions['id'] ?: $tooltipId;
+
+        // Опции для toolTip, доступно коррекция позиционирования
+        // по умолчанию заданы параметры [position => ['x' => 100, 'y' => 210]]
+        $this->toolTipOptions = json_encode(ArrayHelper::merge([
+            'id' => $this->toolTipContainerOptions['id'],
+            'position' => ['x' => 80, 'y' => 120],
+        ], $this->toolTipOptions));
+
+        // Html заголовки и опции для тега svg
+        $this->svgContainerOptions = ArrayHelper::merge([
+            'id' => $svgId,
+            'xmlns' => 'http://www.w3.org/2000/svg',
+            'xmlns:xlink' => 'http://www.w3.org/1999/xlink',
+            'version' => '1.1',
+        ], $this->svgContainerOptions);
+        $this->svgContainerOptions['id'] = $this->svgContainerOptions['id'] ?: $svgId;
     }
 
+    /**
+     * Render SVG
+     */
     public function run()
     {
-        SvgMapWidgetAsset::register($this->getView());
-        return $this->render('default', [
-            'id' => $this->getId(),
-            'type' => ($this->type === self::DATA_SOURCE_ARRAY) ? 'json' : 'url',
-            'data' => $this->data,
-            'showTip' => ($this->showTip) ? 'true' : 'false',
-            'showTools' => ($this->showTools) ? 'true' : 'false',
-            'onClick' =>  ($this->onClick) ? $this->onClick : 'null',
-            'onOver' =>  ($this->onOver) ? $this->onOver : 'null',
-            'onOut' =>  ($this->onOut) ? $this->onOut : 'null',
-        ]);
+        $this->registerAssets();
+        echo Html::beginTag('div', $this->containerOptions) . PHP_EOL;
+        echo $this->render('_zoom2', ['id' => $this->toolsId]) . PHP_EOL;
+        echo Html::tag('div', '', $this->toolTipContainerOptions) . PHP_EOL;
+        echo Html::tag('svg', '', $this->svgContainerOptions) . PHP_EOL;
+        echo Html::endTag('div') . PHP_EOL;
+    }
+
+    /**
+     * Publish and register resource
+     */
+    public function registerAssets()
+    {
+        $view = $this->getView();
+        SvgMapWidgetAsset::register($view);
+        $view->registerJs(new JsExpression("
+            new SvgMap({
+                id: '{$this->id}',
+                svgId: '{$this->svgContainerOptions["id"]}',
+                type: '{$this->type}',
+                data: {$this->data},
+                showTip:  {$this->showTip},
+                onClick: {$this->onClick},
+                onOver: {$this->onOver},
+                onOut: {$this->onOut},
+                toolTipOptions: {$this->toolTipOptions}
+            });
+        "), $view::POS_READY);
     }
 }
 
---
+++ views/_zoom.php
@@ -0,0 +1,9 @@
+<?php
+/* @var $id string */
+?>
+
+<div id="tools_<?= $id ?>" style="position: absolute; z-index: 1; display:inline">
+    <div><i class="glyphicon glyphicon-zoom-in" aria-hidden="true"></i></div>
+    <div><i class="glyphicon glyphicon-refresh" aria-hidden="true"></i></div>
+    <div><i class="glyphicon glyphicon-zoom-out" aria-hidden="true"></i></div>
+</div>
